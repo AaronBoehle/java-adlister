4e1412916b32b7d745f7c75a66126ddee213e63b
AaronBoehle <boehle.aaron@gmail.com>
2019-02-06 16:57:55 -0600

    beginning cookies and sessions exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -14,12 +15,18 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        HttpSession session = request.getSession();
+
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
             response.sendRedirect("/profile");
+            session.setAttribute("admin", true);
         } else {
             response.sendRedirect("/login");
+            session.setAttribute("admin", false);
+
         }
     }
+
 }
